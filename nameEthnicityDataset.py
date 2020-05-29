ec65f7f02b26fc92acc1a3b80e4523e88c08df7c
hollowcodes <teddypeifer@gmail.com>
2020-05-29 16:30:44 +0200

    add doc-strings

--- nameEthnicityDataset.py
+++ nameEthnicityDataset.py
@@ -7,6 +7,12 @@ import numpy as np
 
 class NameEthnicityDatset(torch.utils.data.Dataset):
     def __init__(self, root_dir: str="", class_amount: int=10):
+        """ constructor
+
+        :param str root_dir: dataset path
+        :param int class_amount: amount of classes(/nationalities) in the dataset
+        """
+
         self.root_dir = root_dir
         self.class_amount = class_amount
 
@@ -14,24 +20,26 @@ class NameEthnicityDatset(torch.utils.data.Dataset):
             self.dataset = json.load(f)
     
     def _create_one_hot(self, int_representation: int) -> list:
+        """ create one-hot encoding of the target
+
+        :param int int_representation: class of sample
+        :return list: ie. int_representation = 2 -> [0, 0, 1, ..., 0]
+        """
+
         zero_hot_target = np.zeros((self.class_amount))
         one_hot_target = empty_target[int_representation] = 1
 
         return one_hot_target
 
-    def __getitem__(self, idx: int):
+    # loads sample from dataset, creates one-hot enc. from target and returns
+    def __getitem__(self, idx: int) -> torch.Tensor:
+        """ get sample (batch) from dataset
+
+        :param int idx: index of dataset (iterator of training-loop)
+        :return tensor: preprocessed sample and target
+        """
+        
         sample, target = self.dataset[idx][0], self.dataset[idx][1]
         target = self._create_one_hot(target)
 
         return torch.Tensor(sample), torch.Tensor(target)
-
-
-
-# docstring
-""" creates one-hot representation of class 
-
-:param int_representation: class number n [0; class_amount-1]
-:type int_representation: int
-:rtype: list
-:return: ie, class_amount = 5 ; int_representation = 1 -> [0, 1, 0, 0, 0]
-"""
