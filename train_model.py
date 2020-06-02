45429e80b00dc2f03e71969f6cb09c3be672b77b
hollowcodes <teddypeifer@gmail.com>
2020-06-02 13:39:54 +0200

    created train-structure, added helper functions in utils

--- train_model.py
+++ train_model.py
@@ -1,2 +1,100 @@
-
 """ file to train and evaluate the model """
+
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+import torch
+import torch.utils.data
+import torch.nn as nn
+
+from nameEthnicityDataset import NameEthnicityDatset
+from model import Model
+from utils import create_dataloader, validate_accuracy, show_progress, onehot_to_char
+
+
+class Run:
+    def __init__(self, model_file: str="", dataset_path: str="", epochs: int=10, lr: float=0.001, batch_size: int=32, threshold: float=0.5):
+        self.model_file = model_file
+        self.dataset_path = dataset_path
+        self.train_set, self.validation_set, self.test_set = create_dataloader(dataset_path=self.dataset_path, test_size=0.01, val_size=0.01, batch_size=self.batch_size)
+
+        self.epochs = epochs
+        self.lr = lr
+        self.batch_size = batch_size
+        self.threshold = threshold
+
+    def _prepare_batch(self, batch):
+        """ TODO add batch-padding """
+
+        return batch
+
+    def _validate(self, model, dataset):
+        validation_dataset = dataset
+
+        total_targets, total_predictions = [], []
+
+        for names, targets in tqdm(validation_dataset, desc="validating", ncols=150):
+            names = names.float().cuda()
+            targets = targets.float().cuda()
+
+            predictions = model.eval()(names)
+
+            for i in range(predictions.size()[0]):
+                total_targets.append(targets[i].cpu().detach().numpy())
+                total_predictions.append(predictions[i].cpu().detach().numpy())
+
+        # calculate accuracy
+        accuracy = validate_accuracy(total_targets, total_predictions, threshold=self.threshold)
+
+        # calculate loss
+        criterion = nn.MSELoss()
+        loss = criterion(predictions, targets).item()
+
+        return loss, accuracy
+
+    def train(self, continue_: bool=False):
+        model = Model.cuda()
+
+        if continue_:
+            model.load_state_dict(torch.load(self.model_file))
+
+        criterion = nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
+
+        train_loss_history, train_accuracy_history, val_loss_history, val_accuracy_history = [], [], [], []
+        for epoch in range(1, (self.epochs + 1)):
+
+            epoch_train_loss = []
+            for names, targets in tqdm(self.train_set, desc="epoch", ncols=150):
+                optimizer.zero_grad()
+
+                names, targets = names.cuda(), targets.cuda()
+                names = self._prepare_batch(names)
+
+                predictions = model.train()(names)
+
+                loss = criterion(predictions, targets)
+                loss.backward()
+                optimizer.step()
+
+                epoch_train_loss.append(loss.item())
+        
+            epoch_train_loss, epoch_train_accuracy = np.mean(epoch_loss), self._validate(model, self.train_set)
+            epoch_val_loss, epoch_val_accuracy = self._validate(model, self.validation_set)
+
+            train_loss_history.append(epoch_train_loss); train_accuracy_history.append(epoch_train_accuracy)
+            val_loss_history.append(epoch_val_loss); val_accuracy_history.append(epoch_val_accuracy)
+
+            show_progress(self.epochs, epoch, epoch_train_loss, epoch_train_accuracy, epoch_val_loss, epoch_val_accuracy)
+
+        # TODO add history plot
+
+    def test(self):
+        model = Model.cuda()
+        model.load_state_dict(torch.load(self.model_file))
+
+        accuracy, _ = self._validate(model, self.test_set)
+
+        print(accuracy)
--- utils.py
+++ utils.py
@@ -1,2 +1,134 @@
-
 """ file for small helper functions """
+
+import string
+import numpy as np
+import torch
+import torch.utils.data
+import torch.nn as nn
+
+
+def create_dataloader(dataset_path: str="", test_size: float=0.01, val_size: float=0.01, batch_size: int=32):
+    """ create three dataloader (train, test, validation)
+
+    :param str dataset_path: path to dataset
+    :param float test_size/val_size: test-/validation-percentage of dataset
+    :param int batch_size: batch-size
+    :return torch.Dataloader: train-, test- and val-dataloader
+    """
+
+    dataset = NameEthnicityDatset(dataset_path)
+
+    test_amount, val_amount = int(dataset.__len__() * test_size), int(dataset.__len__() * val_size)
+
+    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [
+        (dataset.__len__() - (test_amount + val_amount)), 
+        test_amount, 
+        val_amount
+    ])
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=batch_size,
+        num_workers=2,
+        shuffle=True,
+    )
+    val_dataloader = torch.utils.data.DataLoader(
+        val_set,
+        batch_size=int(batch_size / 2),
+        num_workers=1,
+        shuffle=True,
+    )
+    test_dataloader = torch.utils.data.DataLoader(
+        test_set,
+        batch_size=int(batch_size / 2),
+        num_workers=1,
+        shuffle=True,
+    )
+
+    return train_dataloader, val_dataloader, test_dataloader
+
+
+def validate_accuracy(y_true, y_pred, threshold: float) -> float:
+    """ calculate the accuracy of predictions
+    
+    :param torch.tensor y_true: targets
+    :param torch.tensor y_pred: predictions
+    :param float threshold: treshold for logit-rounding
+    :return float: accuracy
+    """
+
+    correct_in_batch = 0
+    for i in range(len(y_true)):
+        output, target = y_pred[i], y_true[i]
+        output = [1 if e >= threshold else 0 for e in output]
+
+        if list(target) == output:
+            correct_in_batch += 1
+    
+    return round((100 * correct_in_batch / len(y_true)), 5)
+
+
+def show_progress(epochs: int, epoch: int, loss: float, val_accuracy: float, val_loss: float):
+    """ print training stats
+    
+    :param int epochs: amount of total epochs
+    :param int epoch: current epoch
+    :param float loss: train-loss
+    :param float val_accuracy/val_loss: validation accuracy/loss
+    :return None
+    """
+    epochs = colored(epoch, "cyan", attrs=['bold']) + colored("/", "cyan", attrs=['bold']) + colored(epochs, "cyan", attrs=['bold'])
+    loss = colored(round(loss, 6), "cyan", attrs=['bold'])
+    accuracy = colored(round(val_accuracy, 4), "cyan", attrs=['bold']) + colored("%", "cyan", attrs=['bold'])
+    val_loss = colored(round(val_loss, 6), "cyan", attrs=['bold'])
+    
+    print("epoch {} - loss: {} - val_acc: {} - val_loss: {}".format(epochs, loss, accuracy, val_loss), "\n")
+
+
+def onehot_to_char(one_hot_name: list=[]) -> str:
+    alphabet = string.ascii_lowercase.strip()
+    """ convert one-hot encoded name back to string
+
+    :param list one_hot_name: one-hot enc. name
+    :return str: original string-type name
+    """
+
+    name = ""
+    for one_hot_char in one_hot_name:
+        idx = [i for i, x in enumerate(one_hot_char) if x == 1][0]
+
+        if idx == 26:
+            name += " "
+        elif idx == 27:
+            name += "-"
+        else:
+            name += alphabet[idx]
+
+    return name
+
+
+""" onehot_to_char test
+
+j = np.zeros((28))
+j[9] = 1
+
+o = np.zeros((28))
+o[14] = 1
+
+e = np.zeros((28))
+e[4] = 1
+
+leer = np.zeros((28))
+leer[26] = 1
+
+a = np.zeros((28))
+a[0] = 1
+
+b = np.zeros((28))
+b[1] = 1
+
+c = np.zeros((28))
+c[2] = 1
+
+test_name = [j, o, e, leer, a, b, c]
+print(onehot_to_char(one_hot_name=test_name))"""
